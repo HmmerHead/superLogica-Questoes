5ce4199925ad5b9ff67893b7f8b3b8d423a72df4
HmmerHead <soad_guilherme@hotmail.com>
2022-02-11 12:28:31 -0300

    Alterando estrutura da tabela e criando seed's

--- item1/database/factories/UserFactory.php
+++ item1/database/factories/UserFactory.php
@@ -16,6 +16,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'login' => $this->faker->userName(),
+            'cep' => $this->faker->numberBetween('11111111', '99999999'),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
